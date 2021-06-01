287388af7f48e973ace59bf78a07744552b72c20
whynot1597PC <ja_goldberg@yahoo.com>
2021-06-01 11:39:27 -0700

    remove extra char

--- portfolio-site/src/ProjectCard.js
+++ portfolio-site/src/ProjectCard.js
@@ -23,7 +23,6 @@ function ProjectCard(props) {
       transition={{ duration: 0.5 }}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      w
     >
       <CardActionArea onClick={handleButton}>
         <Card className={classes.card}>
