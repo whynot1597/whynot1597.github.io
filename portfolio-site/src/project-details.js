f257ea3ab555905ba5248c05a96df20805744f34
whynot1597PC <ja_goldberg@yahoo.com>
2021-06-01 11:36:56 -0700

    Fix spelling

--- portfolio-site/src/project-details.js
+++ portfolio-site/src/project-details.js
@@ -2,17 +2,17 @@ const projectDetails = [
   {
     name: "Cognitive System Design",
     description:
-      "This was a group project where we completely designed a Pacman-like agent using approximate q-learning with a feature-based representation. This agent was completely designed by this group. My contribution consisted of implementing the features of the agent and updating its history.",
+      "This was a group project where we created a Pacman-like agent using approximate q-learning with a feature-based representation. This agent was completely designed by this group. My contribution consisted of implementing the features of the agent and updating its history.",
   },
   {
-    name: "Complier",
+    name: "Compilier",
     description:
-      "This was a group project where we designed a computer langauge from scratch. We wrote a grammer, constructed abstract syntax trees, performed semantic analysis, and optimized the resulting code. I contributed to all of these sections in varying capacity. Larger sections of my work are located in the grammar and optimization secions",
+      "This was a group project where we designed a computer langauge from scratch. We wrote a grammer, constructed abstract syntax trees, performed semantic analysis, and optimized the resulting code. I contributed to all of these sections in varying capacities. Larger sections of my work are located in the grammar and optimization sections.",
   },
   {
     name: "Databases",
     description:
-      "This was a group project where we took a sample dataset from Board Game Geek and built a full database sdk around the dataset. We provided functions to users to add ratings and games. We also wrote functions that allowed users to determine useful queries, such as the top rated games.",
+      "This was a group project where we took a sample dataset from Board Game Geek and built a full database SDK around the dataset. We provided functions to users to add ratings and games. We also wrote functions that allowed users to determine useful queries, such as the top rated games. My contribution included organizing the group and ensuring we met deadlines.",
   },
   {
     name: "Intro to AI",
@@ -25,14 +25,14 @@ const projectDetails = [
       "This was a solo project where I created a WebSocket server designed to run on a ESP-8266 Wi-Fi board. This server is used to control an LED light strip. I wrote all the code that is used to create the server as well as send the signals to the LED light strip.",
   },
   {
-    name: "Theory of Compuation",
+    name: "Theory of Computation",
     description:
-      "This was a solo project where I wrote a React-based webapp that allowed users to create Determinisitc Finite Automaton and Nondeterminisitc Finite Automaton. This app also provides a variety of functionality to the user including creating the alphabet and converting between the different automaton.",
+      "This was a solo project where I wrote a React-based webapp that allowed users to create Deterministic Finite Automatons and Nondeterministic Finite Automatons. This app also provides a variety of functionality to the user including creating the alphabet and converting between the different automaton.",
   },
   {
     name: "Thesis",
     description:
-      "This was a group project that created a React-based webapp that trained users on how to use a synthesizer online. I was responsible for creating all the webpages and creating a template for all the information. I also used Google's firebase product to store and update user information for easy navigation and credential storing.",
+      "This was a group project that created a React-based webapp that trained users on how to use a synthesizer online. I was responsible for creating all the webpages and creating a template for all the information. I also used Google's Firebase product to store and update user information for easy navigation and credential storing.",
   },
   {
     name: "portfolio-site",
